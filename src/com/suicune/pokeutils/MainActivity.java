154b582265d6f4a8f977caa259ab3f5cbcdc97d0
Denís <lapuente@abbyy.com>
2013-03-01 17:09:32 +0100

    Move database creation to thread instead of async task. SD Write permission must be removed

--- src/com/suicune/pokeutils/MainActivity.java
+++ src/com/suicune/pokeutils/MainActivity.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
@@ -35,8 +35,6 @@ public class MainActivity extends TabCompatActivity {
 		super.onCreate(savedInstanceState);
 		prefs = PreferenceManager.getDefaultSharedPreferences(this);
 		if (prefs.getBoolean(SettingsActivity.FIRST_RUN, true)) {
-			Toast.makeText(this, R.string.first_run_load, Toast.LENGTH_LONG)
-					.show();
 			makeFirstRun();
 			prefs.edit().putBoolean(SettingsActivity.FIRST_RUN, false).commit();
 		}
@@ -138,168 +136,185 @@ public class MainActivity extends TabCompatActivity {
 	}
 
 	private void makeFirstRun() {
-		new InsertDataTask().execute(null, null);
+
+		Toast.makeText(this, R.string.first_run_load, Toast.LENGTH_LONG).show();
+
+		 new Thread(new InsertDataTask(this)).start();
 	}
 
-	private void setDatabase() {
-		loadNatures();
-		loadAbilities();
-		loadPokemon();
-	}
+	 private class InsertDataTask implements Runnable {
+		private Context mContext;
 
-	public void loadPokemon() {
-		ArrayList<String> elements = new ArrayList<String>();
-		elements.add(PokeContract.PokemonName.NUMBER);
-		elements.add(PokeContract.PokemonName.FORM);
-		elements.add(PokeContract.PokemonName.NAME);
-		elements.add(PokeContract.PokemonType1.TYPE);
-		elements.add(PokeContract.PokemonType2.TYPE);
-		elements.add(PokeContract.PokemonBaseStats.BASE_HP);
-		elements.add(PokeContract.PokemonBaseStats.BASE_ATT);
-		elements.add(PokeContract.PokemonBaseStats.BASE_DEF);
-		elements.add(PokeContract.PokemonBaseStats.BASE_SPATT);
-		elements.add(PokeContract.PokemonBaseStats.BASE_SPDEF);
-		elements.add(PokeContract.PokemonBaseStats.BASE_SPEED);
-		elements.add(PokeContract.PokemonAbility1.ABILITY_1);
-		elements.add(PokeContract.PokemonAbility2.ABILITY_2);
-		elements.add(PokeContract.PokemonAbilityDW.ABILITY_DW);
+		public InsertDataTask(Context context) {
+			mContext = context;
+			run();
+		}
 
-		ArrayList<HashMap<String, String>> result = DBReader.readDB(
-				getResources().openRawResource(R.raw.pokemon), elements);
-		for (int i = 0; i < result.size(); i++) {
-			ContentValues pokemonValues = new ContentValues();
-			ContentValues pokemonStatsValues = new ContentValues();
-			ContentValues type1Values = new ContentValues();
-			ContentValues type2Values = new ContentValues();
-			ContentValues ability1Values = new ContentValues();
-			ContentValues ability2Values = new ContentValues();
-			ContentValues abilityDWValues = new ContentValues();
-			HashMap<String, String> pokemon = result.get(i);
-			String number = pokemon.get(PokeContract.PokemonName.NUMBER);
-			String form = pokemon.get(PokeContract.PokemonName.FORM);
-			String name = pokemon.get(PokeContract.PokemonName.NAME);
-			String type1 = pokemon.get(PokeContract.PokemonType1.TYPE);
-			String type2 = pokemon.get(PokeContract.PokemonType2.TYPE);
-			String ability1 = pokemon
-					.get(PokeContract.PokemonAbility1.ABILITY_1);
-			String ability2 = pokemon
-					.get(PokeContract.PokemonAbility2.ABILITY_2);
-			String abilityDW = pokemon
-					.get(PokeContract.PokemonAbilityDW.ABILITY_DW);
-			String baseHP = pokemon.get(PokeContract.PokemonBaseStats.BASE_HP);
-			String baseAtt = pokemon
-					.get(PokeContract.PokemonBaseStats.BASE_ATT);
-			String baseDef = pokemon
-					.get(PokeContract.PokemonBaseStats.BASE_DEF);
-			String baseSpAtt = pokemon
-					.get(PokeContract.PokemonBaseStats.BASE_SPATT);
-			String baseSpDef = pokemon
-					.get(PokeContract.PokemonBaseStats.BASE_SPDEF);
-			String baseSpeed = pokemon
-					.get(PokeContract.PokemonBaseStats.BASE_SPEED);
-			pokemonValues.put(PokeContract.PokemonName.NUMBER, number);
-			pokemonStatsValues.put(PokeContract.PokemonName.NUMBER, number);
-			type1Values.put(PokeContract.PokemonName.NUMBER, number);
-			type2Values.put(PokeContract.PokemonName.NUMBER, number);
-			ability1Values.put(PokeContract.PokemonName.NUMBER, number);
-			ability2Values.put(PokeContract.PokemonName.NUMBER, number);
-			abilityDWValues.put(PokeContract.PokemonName.NUMBER, number);
-			pokemonValues.put(PokeContract.PokemonName.FORM, form);
-			pokemonStatsValues.put(PokeContract.PokemonName.FORM, form);
-			type1Values.put(PokeContract.PokemonName.FORM, form);
-			type2Values.put(PokeContract.PokemonName.FORM, form);
-			ability1Values.put(PokeContract.PokemonName.FORM, form);
-			ability2Values.put(PokeContract.PokemonName.FORM, form);
-			abilityDWValues.put(PokeContract.PokemonName.FORM, form);
-			pokemonValues.put(PokeContract.PokemonName.NAME, name);
-			pokemonStatsValues.put(PokeContract.PokemonBaseStats.BASE_HP,
-					baseHP);
-			pokemonStatsValues.put(PokeContract.PokemonBaseStats.BASE_ATT,
-					baseAtt);
-			pokemonStatsValues.put(PokeContract.PokemonBaseStats.BASE_DEF,
-					baseDef);
-			pokemonStatsValues.put(PokeContract.PokemonBaseStats.BASE_SPATT,
-					baseSpAtt);
-			pokemonStatsValues.put(PokeContract.PokemonBaseStats.BASE_SPDEF,
-					baseSpDef);
-			pokemonStatsValues.put(PokeContract.PokemonBaseStats.BASE_SPEED,
-					baseSpeed);
-			type1Values.put(PokeContract.PokemonType1.TYPE, type1);
-			type2Values.put(PokeContract.PokemonType2.TYPE, type2);
-			ability1Values
-					.put(PokeContract.PokemonAbility1.ABILITY_1, ability1);
-			ability2Values
-					.put(PokeContract.PokemonAbility2.ABILITY_2, ability2);
-			abilityDWValues.put(PokeContract.PokemonAbilityDW.ABILITY_DW,
-					abilityDW);
+		public void run() {
+			loadNatures();
+			loadPokemon();
+			loadAbilities();
+			return;
+		}
 
-			getContentResolver().insert(
-					PokeContract.PokemonName.CONTENT_POKEMON_NAME,
-					pokemonValues);
-			getContentResolver().insert(
+		public void loadPokemon() {
+			ArrayList<String> elements = new ArrayList<String>();
+			elements.add(PokeContract.PokemonName.NUMBER);
+			elements.add(PokeContract.PokemonName.FORM);
+			elements.add(PokeContract.PokemonName.NAME);
+			elements.add(PokeContract.PokemonType1.TYPE);
+			elements.add(PokeContract.PokemonType2.TYPE);
+			elements.add(PokeContract.PokemonBaseStats.BASE_HP);
+			elements.add(PokeContract.PokemonBaseStats.BASE_ATT);
+			elements.add(PokeContract.PokemonBaseStats.BASE_DEF);
+			elements.add(PokeContract.PokemonBaseStats.BASE_SPATT);
+			elements.add(PokeContract.PokemonBaseStats.BASE_SPDEF);
+			elements.add(PokeContract.PokemonBaseStats.BASE_SPEED);
+			elements.add(PokeContract.PokemonAbility1.ABILITY_1);
+			elements.add(PokeContract.PokemonAbility2.ABILITY_2);
+			elements.add(PokeContract.PokemonAbilityDW.ABILITY_DW);
+
+			ArrayList<HashMap<String, String>> result = DBReader.readDB(
+					getResources().openRawResource(R.raw.pokemon), elements);
+			ContentValues[] pokemons = new ContentValues[result.size()];
+			ContentValues[] stats = new ContentValues[result.size()];
+			ContentValues[] type1s = new ContentValues[result.size()];
+			ContentValues[] type2s = new ContentValues[result.size()];
+			ContentValues[] ability1s = new ContentValues[result.size()];
+			ContentValues[] ability2s = new ContentValues[result.size()];
+			ContentValues[] abilitydws = new ContentValues[result.size()];
+			for (int i = 0; i < result.size(); i++) {
+				ContentValues pokemonValues = new ContentValues();
+				ContentValues pokemonStatsValues = new ContentValues();
+				ContentValues type1Values = new ContentValues();
+				ContentValues type2Values = new ContentValues();
+				ContentValues ability1Values = new ContentValues();
+				ContentValues ability2Values = new ContentValues();
+				ContentValues abilityDWValues = new ContentValues();
+				HashMap<String, String> pokemon = result.get(i);
+				String number = pokemon.get(PokeContract.PokemonName.NUMBER);
+				String form = pokemon.get(PokeContract.PokemonName.FORM);
+				String name = pokemon.get(PokeContract.PokemonName.NAME);
+				String type1 = pokemon.get(PokeContract.PokemonType1.TYPE);
+				String type2 = pokemon.get(PokeContract.PokemonType2.TYPE);
+				String ability1 = pokemon
+						.get(PokeContract.PokemonAbility1.ABILITY_1);
+				String ability2 = pokemon
+						.get(PokeContract.PokemonAbility2.ABILITY_2);
+				String abilityDW = pokemon
+						.get(PokeContract.PokemonAbilityDW.ABILITY_DW);
+				String baseHP = pokemon
+						.get(PokeContract.PokemonBaseStats.BASE_HP);
+				String baseAtt = pokemon
+						.get(PokeContract.PokemonBaseStats.BASE_ATT);
+				String baseDef = pokemon
+						.get(PokeContract.PokemonBaseStats.BASE_DEF);
+				String baseSpAtt = pokemon
+						.get(PokeContract.PokemonBaseStats.BASE_SPATT);
+				String baseSpDef = pokemon
+						.get(PokeContract.PokemonBaseStats.BASE_SPDEF);
+				String baseSpeed = pokemon
+						.get(PokeContract.PokemonBaseStats.BASE_SPEED);
+				pokemonValues.put(PokeContract.PokemonName.NUMBER, number);
+				pokemonStatsValues.put(PokeContract.PokemonName.NUMBER, number);
+				type1Values.put(PokeContract.PokemonName.NUMBER, number);
+				type2Values.put(PokeContract.PokemonName.NUMBER, number);
+				ability1Values.put(PokeContract.PokemonName.NUMBER, number);
+				ability2Values.put(PokeContract.PokemonName.NUMBER, number);
+				abilityDWValues.put(PokeContract.PokemonName.NUMBER, number);
+				pokemonValues.put(PokeContract.PokemonName.FORM, form);
+				pokemonStatsValues.put(PokeContract.PokemonName.FORM, form);
+				type1Values.put(PokeContract.PokemonName.FORM, form);
+				type2Values.put(PokeContract.PokemonName.FORM, form);
+				ability1Values.put(PokeContract.PokemonName.FORM, form);
+				ability2Values.put(PokeContract.PokemonName.FORM, form);
+				abilityDWValues.put(PokeContract.PokemonName.FORM, form);
+				pokemonValues.put(PokeContract.PokemonName.NAME, name);
+				pokemonStatsValues.put(PokeContract.PokemonBaseStats.BASE_HP,
+						baseHP);
+				pokemonStatsValues.put(PokeContract.PokemonBaseStats.BASE_ATT,
+						baseAtt);
+				pokemonStatsValues.put(PokeContract.PokemonBaseStats.BASE_DEF,
+						baseDef);
+				pokemonStatsValues.put(
+						PokeContract.PokemonBaseStats.BASE_SPATT, baseSpAtt);
+				pokemonStatsValues.put(
+						PokeContract.PokemonBaseStats.BASE_SPDEF, baseSpDef);
+				pokemonStatsValues.put(
+						PokeContract.PokemonBaseStats.BASE_SPEED, baseSpeed);
+				type1Values.put(PokeContract.PokemonType1.TYPE, type1);
+				type2Values.put(PokeContract.PokemonType2.TYPE, type2);
+				ability1Values.put(PokeContract.PokemonAbility1.ABILITY_1,
+						ability1);
+				ability2Values.put(PokeContract.PokemonAbility2.ABILITY_2,
+						ability2);
+				abilityDWValues.put(PokeContract.PokemonAbilityDW.ABILITY_DW,
+						abilityDW);
+				pokemons[i] = pokemonValues;
+				stats[i] = pokemonStatsValues;
+				type1s[i] = type1Values;
+				type2s[i] = type2Values;
+				ability1s[i] = ability1Values;
+				ability2s[i] = ability2Values;
+				abilitydws[i] = abilityDWValues;
+			}
+
+			mContext.getContentResolver().bulkInsert(
+					PokeContract.PokemonName.CONTENT_POKEMON_NAME, pokemons);
+			mContext.getContentResolver().bulkInsert(
 					PokeContract.PokemonBaseStats.CONTENT_POKEMON_BASE_STATS,
-					pokemonStatsValues);
-			getContentResolver().insert(
-					PokeContract.PokemonType1.CONTENT_POKEMON_TYPE_1,
-					type1Values);
-			getContentResolver().insert(
-					PokeContract.PokemonType2.CONTENT_POKEMON_TYPE_2,
-					type2Values);
-			getContentResolver().insert(
+					stats);
+			mContext.getContentResolver().bulkInsert(
+					PokeContract.PokemonType1.CONTENT_POKEMON_TYPE_1, type1s);
+			mContext.getContentResolver().bulkInsert(
+					PokeContract.PokemonType2.CONTENT_POKEMON_TYPE_2, type2s);
+			mContext.getContentResolver().bulkInsert(
 					PokeContract.PokemonAbility1.CONTENT_POKEMON_ABILITY_1,
-					ability1Values);
-			getContentResolver().insert(
+					ability1s);
+			mContext.getContentResolver().bulkInsert(
 					PokeContract.PokemonAbility2.CONTENT_POKEMON_ABILITY_2,
-					ability2Values);
-			getContentResolver().insert(
+					ability2s);
+			mContext.getContentResolver().bulkInsert(
 					PokeContract.PokemonAbilityDW.CONTENT_POKEMON_ABILITY_DW,
-					abilityDWValues);
+					abilitydws);
 		}
-	}
 
-	public void loadNatures() {
-		ArrayList<String> elements = new ArrayList<String>();
-		elements.add(PokeContract.Natures.NAME);
+		public void loadNatures() {
+			ArrayList<String> elements = new ArrayList<String>();
+			elements.add(PokeContract.Natures.NAME);
 
-		ArrayList<HashMap<String, String>> result = DBReader.readDB(
-				getResources().openRawResource(R.raw.natures), elements);
+			ArrayList<HashMap<String, String>> result = DBReader.readDB(
+					getResources().openRawResource(R.raw.natures), elements);
 
-		for (int i = 0; i < result.size(); i++) {
-			ContentValues values = new ContentValues();
-			HashMap<String, String> nature = result.get(i);
-			for (int j = 0; j < nature.size(); j++) {
-				values.put(elements.get(j), nature.get(elements.get(j)));
+			for (int i = 0; i < result.size(); i++) {
+				ContentValues values = new ContentValues();
+				HashMap<String, String> nature = result.get(i);
+				for (int j = 0; j < nature.size(); j++) {
+					values.put(elements.get(j), nature.get(elements.get(j)));
+				}
+				mContext.getContentResolver().insert(
+						PokeContract.Natures.CONTENT_NATURE, values);
 			}
-			getContentResolver().insert(PokeContract.Natures.CONTENT_NATURE,
-					values);
 		}
-	}
 
-	public void loadAbilities() {
-		ArrayList<String> elements = new ArrayList<String>();
-		elements.add(PokeContract.Abilities.ID);
-		elements.add(PokeContract.Abilities.NAME);
-		elements.add(PokeContract.Abilities.DESCRIPTION);
+		public void loadAbilities() {
+			ArrayList<String> elements = new ArrayList<String>();
+			elements.add(PokeContract.Abilities.ID);
+			elements.add(PokeContract.Abilities.NAME);
+			elements.add(PokeContract.Abilities.DESCRIPTION);
 
-		ArrayList<HashMap<String, String>> result = DBReader.readDB(
-				getResources().openRawResource(R.raw.abilities), elements);
+			ArrayList<HashMap<String, String>> result = DBReader.readDB(
+					getResources().openRawResource(R.raw.abilities), elements);
 
-		for (int i = 0; i < result.size(); i++) {
-			ContentValues values = new ContentValues();
-			HashMap<String, String> ability = result.get(i);
-			for (int j = 0; j < ability.size(); j++) {
-				values.put(elements.get(j), ability.get(elements.get(j)));
+			for (int i = 0; i < result.size(); i++) {
+				ContentValues values = new ContentValues();
+				HashMap<String, String> ability = result.get(i);
+				for (int j = 0; j < ability.size(); j++) {
+					values.put(elements.get(j), ability.get(elements.get(j)));
+				}
+				mContext.getContentResolver().insert(
+						PokeContract.Abilities.CONTENT_ABILITY, values);
 			}
-			getContentResolver().insert(PokeContract.Abilities.CONTENT_ABILITY,
-					values);
-		}
-	}
-
-	private class InsertDataTask extends AsyncTask<Void, Void, Void> {
-		@Override
-		protected Void doInBackground(Void... params) {
-			setDatabase();
-			return null;
 		}
 	}
 }
